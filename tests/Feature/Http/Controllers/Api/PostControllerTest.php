f7ebe3600e61b5e270512fd5611d7fd8e7680635
jfguerra <jfguerra@userena.cl>
2021-09-06 18:45:39 -0300

    Api de ejemplo

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\User;
+use App\Post;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => 'El post de la prueba'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'El post de la prueba'])
+            ->assertStatus(201); //OK, creado un recurso
+        
+        $this->assertDatabaseHas('posts', ['title' => 'El post de la prueba']);
+    }
+
+    public function test_validate_title()
+    {
+        $user = factory(User::class)->create();
+
+        //No quiero enviar el titulo
+        $response = $this->actingAs($user, 'api')->json('POST', '/api/posts', [
+            'title' => ''
+        ]);
+
+        //HTTP 422 la solicitud está bien hecha, pero fue imposible completarla
+        $response->assertStatus(422) 
+            ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show()
+    {
+        $user = factory(User::class)->create();
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs($user, 'api')->json('GET', "/api/posts/$post->id");
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => $post->title])
+            ->assertStatus(200); //OK
+    }
+
+    public function test_404_show()
+    {
+        $user = factory(User::class)->create();
+
+        //$post = factory(Post::class)->create(); el post no existe
+
+        $response = $this->actingAs($user, 'api')->json('GET', '/api/posts/1000');
+
+        $response->assertStatus(404); //OK
+    }
+
+    public function test_update()
+    {
+        $user = factory(User::class)->create();
+
+        //$this->withoutExceptionHandling();
+        $post = factory(Post::class)->create();
+        $response = $this->actingAs($user, 'api')->json('PUT', "/api/posts/$post->id", [
+            'title' => 'nuevo'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'nuevo'])
+            ->assertStatus(200); //OK
+        
+        $this->assertDatabaseHas('posts', ['title' => 'nuevo']);//Revisa en bd que esté guardado
+    }
+
+    public function test_delete()
+    {
+        $user = factory(User::class)->create();
+
+        //$this->withoutExceptionHandling();
+        $post = factory(Post::class)->create();
+        $response = $this->actingAs($user, 'api')->json('DELETE', "/api/posts/$post->id");
+
+        $response->assertSee(null)
+            ->assertStatus(204); //Sin contenido
+        
+        $this->assertDatabaseMissing('posts', ['id' => $post->id]);//
+    }
+
+    public function test_index()
+    {
+        $user = factory(User::class)->create();
+
+        factory(Post::class, 5)->create();
+
+        $response = $this->actingAs($user, 'api')->json('GET', '/api/posts');
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => ['id', 'title', 'created_at', 'updated_at']
+            ]
+        ])->assertStatus(200);
+    }
+
+    public function test_guest()
+    {
+        $this->json('GET',    '/api/posts')->assertStatus(401);
+        $this->json('POST',   '/api/posts')->assertStatus(401);
+        $this->json('GET',    '/api/posts/1000')->assertStatus(401);
+        $this->json('PUT',    '/api/posts/1000')->assertStatus(401);
+        $this->json('DELETE', '/api/posts/1000')->assertStatus(401);
+    }
+}
